f2f10a7b0afa1f1c92a58143f020065c03f2e74c
vy <vy.nguyen@vleisure.com>
2023-11-02 20:46:24 +0700

    i

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import morgan from "morgan";
 import rootRoute from "./routes/index.js";
 import databaseService from "./services/database.service.js";
 import { defaultErrorHandler } from "./middlewares/error.middleware.js";
+import cors from "cors";
 databaseService.connect();
 const PORT = 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
