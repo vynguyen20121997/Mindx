b5dc96422c67b7f6ca3e7edbce8d5934eff64a75
vy <vy.nguyen@vleisure.com>
2023-11-02 16:42:14 +0700

    hehe

--- middlewares/validate.middleware.js
+++ middlewares/validate.middleware.js
@@ -1,8 +1,28 @@
-const validateMdw = (schema) => (req, res, next) => {
-  try {
-  } catch (error) {}
-};
-const registerSchema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
+import * as Yup from "Yup";
+
+const registerSchema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().min(6).required(),
 });
+
+const loginShema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().min(6).required(),
+});
+
+const AuthValidator = {
+  registerSchema,
+  loginShema,
+};
+export default AuthValidator;
+
+export const validateMdw = (schema) => async (req, res, next) => {
+  try {
+    await schema.validate(req.body);
+    next();
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
 import AuthController from "../controller/auth.controler.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import AuthValidator, {
+  validateMdw,
+} from "../middlewares/validate.middleware.js";
 
 const router = Router();
 
-router.post("/login", AuthController.loginControler);
-router.post("/register", AuthController.registerController);
+router.post(
+  "/login",
+  validateMdw(AuthValidator.loginShema),
+  AuthController.loginControler
+);
+router.post(
+  "/register",
+  validateMdw(AuthValidator.registerSchema),
+  AuthController.registerController
+);
 router.get("/current-user", authMiddleware, AuthController.fetchCurrentUser);
 
 export default router;
